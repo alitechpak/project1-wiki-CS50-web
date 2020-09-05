e98f029e8c7910550c17b9e8bbb004e68f6f6f56
alitechpak <alitechbeyond@gmail.com>
2020-09-05 21:17:32 +0500

    added feature click on index.html

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,10 +1,20 @@
 from django.shortcuts import render
 
 from . import util
-
+import markdown2
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
 
+def topic(request, md_entry):
+    entry = util.get_entry(md_entry)
+    #   If entry doesn't exist
+    if not entry:
+        return render(request, "encyclopedia/error.html")
+    else:
+        return render(request, "encyclopedia/entry.html", {
+            "title": md_entry,
+            "content": markdown2.markdown(entry)
+        })
