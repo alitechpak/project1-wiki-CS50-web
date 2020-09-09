62dbaa29e5e62e197d5b6675a8c03c39c23428b8
alitechpak <alitechbeyond@gmail.com>
2020-09-09 18:46:37 +0500

    implemented search function

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 
 from . import views
 
+app_name = "wiki"
 urlpatterns = [
     path("", views.index, name="index"),
     path("wiki/<str:md_entry>", views.topic, name="topic"),
+    path("/", views.search, name="search"),
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
 
 from . import util
 import markdown2
@@ -17,4 +18,26 @@ def topic(request, md_entry):
         return render(request, "encyclopedia/entry.html", {
             "title": md_entry,
             "content": markdown2.markdown(entry)
+        })
+
+def search(request):
+    # access the searched string and convert into lowercase
+    query = request.GET.get("q").lower()
+    
+    # all entries list in lowercase
+    # this code snippet took from "https://medium.com/@gilwellm/ho-to-convert-a-list-into-upper-or-lowercase-in-python-3-f2c4bbb71e5a"
+    entries_list = [x.lower() for x in util.list_entries()]
+    q_list = []
+    #   check about query existance
+    if query in entries_list:
+        # Returns function "topic" which get searched query as 2nd argument
+        return topic(request, request.GET.get("q"))
+
+    else:
+        for md in util.list_entries():
+            if query in md.lower():
+                q_list.append(md)
+
+        return render(request, "encyclopedia/search.html", {
+            "q_list": q_list,
         })
